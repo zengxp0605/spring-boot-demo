be7ab4690f8c332fa7422efcbed2e8973baa4f31
jasonzeng <jasonzeng@pa>
2019-08-26 18:55:11 +0800

    master:addDemo

---
+++ 00_basejava/src/main/java/com/jason/basejava/thread/Test.java
@@ -0,0 +1,24 @@
+package com.jason.basejava.thread;
+
+import java.util.Date;
+
+public class Test {
+
+    public static void main(String[] args) {
+        for(int i =0; i< 10; i++){
+            System.out.println("Now: " + new Date());
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        int total = 0;
+        for(int i =0; i<=10000; i++){
+            total ++;
+        }
+        System.out.println("End total: " + total);
+
+    }
+}
