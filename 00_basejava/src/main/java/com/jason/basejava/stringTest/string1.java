251852e96973f8a6fb86c56df82a30d3e1a57c4c
jasonzeng <jasonzeng@pa>
2019-11-11 14:37:35 +0800

    master:add-demo

---
+++ 00_basejava/src/main/java/com/jason/basejava/stringTest/string1.java
@@ -0,0 +1,50 @@
+package com.jason.basejava.stringTest;
+
+
+public class string1 {
+
+    public static void main(String[] args) {
+        // 字符串反转
+        reverseString();
+
+        /**  String 类的常用方法 */
+        // 构造方法
+        stringConstructor();
+    }
+
+    /**
+     * String类构造方法
+     */
+    public static void stringConstructor(){
+        // 用已知的字符串value创建一个String对象
+        String str1 = new String("test-str");
+        String str2 = new String(str1);
+        System.out.println("str2: " + str2);
+
+        // 用字符数组value创建一个String对象。
+        char c = 'c';
+        char[] arr = {'a', 'b', c, 'd'};
+        String str3 = new String(arr);
+        System.out.println("str3: " + str3); // str3: abcd
+
+        // 用字符数组chars的startIndex开始的numChars个字符创建一个String对象。
+        String str4 = new String(arr, 1, 2); //
+        System.out.println("str4: " + str4);
+
+        // 用比特数组values创建一个String对象。
+        byte[] bytes = new byte[]{65, 66, 97, 98}; // ASCII码 ABab
+        String str5 = new String(bytes);
+        System.out.println("str5: " + str5);
+
+    }
+
+    /**
+     * 字符串反转
+     */
+    public static void reverseString(){
+        String str = "zouzou-lover";
+        String reverseStr = new StringBuffer(str).reverse().toString();
+        System.out.println("字符串反转前: " + str);
+        System.out.println("字符串反转后: " + reverseStr);
+    }
+}
